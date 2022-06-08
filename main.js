ee56997bf2935f9cca51ac302de50a7c7a83a079
Shahjalal Sikder <shah.sikder55@gmail.com>
2022-06-08 18:07:04 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('#btn');
+const h1 = document.querySelector('#header');
+
+button.addEventListener('click', function () {
+    const newColor = randColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+
+
+const randColor = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
